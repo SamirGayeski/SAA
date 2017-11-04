6e698fd249b69f788c0219537986063f197dc942
Samir Pedro Gayeski <samirpgayeski@gmail.com>
2017-11-03 23:23:38 -0200

    Version15

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -20,9 +20,57 @@ class ReportController extends Controller{
         ];
     }
 
-    public function index()
-    {
-        $input = public_path() . '/reports/Pacientes.jasper';
+    public function index(){
+
+        $input = public_path() .'/reports/Atendimentos.jasper';
+        $output = public_path() . '/reports/Atendimentos';
+        $options = [
+            'format' => ['pdf'],
+            'locale' => 'pt_BR',
+            'params' => ['datainicial'=>'2017-08-01', 'datafinal'=>'2017-10-30'],
+            'db_connection' => $this->getDatabaseConfig()
+        ];
+
+        $jasper = new PHPJasper;
+
+        $jasper->process($input, $output, $options)->execute();
+        $file = $output . '.pdf';
+        $path = $file;
+
+        $file = file_get_contents($file);
+        unlink($path);
+        return response($file, 200)
+            ->header('Content-Type', 'application/pdf')
+            ->header('Content-Disposition', 'inline; filename="Atendimentos.pdf"');
+    }
+
+    public function agendamentos(){
+
+        $input = public_path() .'/reports/Agendamentos.jasper';
+        $output = public_path() . '/reports/Agendamentos';
+        $options = [
+            'format' => ['pdf'],
+            'locale' => 'pt_BR',
+            'params' => ['status'=>'Atendido'],
+            'db_connection' => $this->getDatabaseConfig()
+        ];
+
+        $jasper = new PHPJasper;
+
+        $jasper->process($input, $output, $options)->execute();
+        $file = $output . '.pdf';
+        $path = $file;
+
+        $file = file_get_contents($file);
+        unlink($path);
+        return response($file, 200)
+            ->header('Content-Type', 'application/pdf')
+            ->header('Content-Disposition', 'inline; filename="Agendamentos.pdf"');
+    }
+
+    public function paciente(){
+
+        $input = public_path() .'/reports/Pacientes.jasper';
         $output = public_path() . '/reports/Pacientes';
         $options = [
             'format' => ['pdf'],
